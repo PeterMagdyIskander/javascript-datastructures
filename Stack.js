0711e6dc27e97d2d9b3d8307f739ba7909dd4487
Peter Iskander <petermagdyiskander@gmail.com>
2023-01-14 20:23:08 +0200

    added stack ds

---
+++ Stack.js
@@ -0,0 +1,38 @@
+class Stack {
+    stackArr = [];
+
+    length() {
+        return this.stackArr.length;
+    }
+    push(num) {
+        this.stackArr.push(num);
+    }
+    empty() {
+        return this.stackArr.length == 0
+    }
+    pop() {
+        let item = this.top();
+        if (item) {
+            this.stackArr.splice(this.stackArr.length - 1, 1);
+        }
+        return item;
+    }
+    top() {
+        if (!this.empty()) {
+            let item = this.stackArr[this.stackArr.length - 1];
+            return item;
+        }
+        else {
+            return -1;
+        }
+    }
+}
+
+let s = new Stack();
+console.log(s.stackArr)
+s.push(1)
+console.log(s.stackArr)
+console.log('top', s.top())
+console.log(s.stackArr)
+console.log('pop', s.pop())
+console.log(s.stackArr)
